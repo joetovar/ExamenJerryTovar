9db0f30bc8e4cbe4d9b0c7a112888d04c56b3818
Jerry Tovar <65546801+joetovar@users.noreply.github.com>
2023-07-19 21:17:31 -0500

    update pagoRol

--- examen/src/main/java/ec/edu/espe/arquitectura/examen/repository/pagoRolRepository.java
+++ examen/src/main/java/ec/edu/espe/arquitectura/examen/repository/pagoRolRepository.java
@@ -1,4 +1,5 @@
 package ec.edu.espe.arquitectura.examen.repository;
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import ec.edu.espe.arquitectura.examen.model.pagoRol;
 
 @Repository
 public interface pagoRolRepository extends MongoRepository<pagoRol, String> {
+     List<pagoRol> findByRucEmpresaAndMes(String rucEmpresa, String mes);
 }
